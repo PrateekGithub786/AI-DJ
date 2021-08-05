1b2769980eefd2105a06d871f8fd00b10d98f209
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-08-05 16:58:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ leftWristy = 0;
 rightWristx = 0;
 rightWristy = 0;
 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function setup(){
     canvas = createCanvas(600, 500);
@@ -25,6 +27,11 @@ function modelLaoded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Left Wrist Score = " + scoreLeftWrist + "Right Wrist Score = " + scoreRightWrist);
+
         leftWristx = results[0].pose.leftWrist.x;
         leftWristy = results[0].pose.leftWrist.y;
         console.log("Left Wrist x = " + leftWristx + "Left Wrist y = " + leftWristy);
@@ -47,4 +54,17 @@ function play(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FFFFFF");
+    stroke("#000000");
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristx, leftWristy, 20);
+        InNumberLeftWristy = Number(leftWristy);
+        removeDecimals = floor(InNumberLeftWristy);
+        volume = removeDecimals/500;
+        song.setVolume(volume);
+        document.getElementById("volume_button").innerHTML = "Volume " + volume;
+    }
+
 }
